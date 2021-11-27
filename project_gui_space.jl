f633a9b0293ab44a08d72313eaa1c457264325b4
Catherine <catherine.cheu@uconn.edu>
2021-11-27 13:12:02 -0500

    Expanding GUI Window

--- project_gui_space.jl
+++ project_gui_space.jl
@@ -3,3 +3,21 @@ tk = pyimport("tkinter")
 pyimport("time")
 
 inpar = Dict("mass1" => 0, "mass2" => 0, "distance" => 1)
+heightwin = 600
+widthwin = 600
+
+@pydef mutable struct GuiApp <: tk.Tk
+    __init__(self) = begin
+        tk.Tk.__init__(self)
+        guiframe = tk.Frame(self)
+        guiframe.pack(fill = "both", expand = true)
+
+        for a in ("IntroScreen", "AnimScreen")
+            print(a)
+        end
+    end
+end
+
+app = GuiApp()
+app.geometry("600x600")
+app.mainloop()
