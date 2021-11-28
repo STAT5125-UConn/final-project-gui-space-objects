0dda1d2e1ea7b8c9f730cd3591839b502e57af35
Catherine <catherine.cheu@uconn.edu>
2021-11-27 19:32:20 -0500

    Updating parameters

--- project_gui_space.jl
+++ project_gui_space.jl
@@ -3,18 +3,17 @@ tk = pyimport("tkinter")
 pyimport("time")
 
 inpar = Dict("mass1" => 0, "mass2" => 0, "distance" => 1)
+time_refresh = 0.01
 heightwin = 600
 widthwin = 600
+animwinheight = 200
+animwinwidth = 200
 
 @pydef mutable struct GuiApp <: tk.Tk
     __init__(self) = begin
         tk.Tk.__init__(self)
         guiframe = tk.Frame(self)
         guiframe.pack(fill = "both", expand = true)
-
-        for a in ("IntroScreen", "AnimScreen")
-            print(a)
-        end
     end
 end
 
