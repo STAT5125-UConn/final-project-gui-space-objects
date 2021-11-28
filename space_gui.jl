875ce2684ade6c2ba0e6f3cebf5384d29ce5dfd7
Catherine <catherine.cheu@uconn.edu>
2021-11-28 13:45:37 -0500

    Updating GUI

---
+++ space_gui.jl
@@ -0,0 +1,22 @@
+using PyCall
+tk = pyimport("tkinter")
+timemod = pyimport("time")
+
+inpar = Dict("mass1" => 0, "mass2" => 0, "r" => 2)
+time_delay = 0.01
+m1startx = 150
+m1starty = 150
+Height = 600
+Width = 600
+aniheight = 300
+aniwidth = 300
+
+@pydef mutable struct Guiapp <: tk.Tk
+    __init__(self) = begin
+        tk.Tk.__init__(self)
+        guiframe = tk.Frame(self, height = Height, width = Width)
+        guiframe.pack(fill = "both", expand = true)
+        canres = tk.Canvas(guiframe, background = "white", height = aniheight, width = aniwidth)
+        canres.place(relx = 0.25, rely = 0.25)
+    end
+end
