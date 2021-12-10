963261180b92458aacd1a8417d3bbd4aec2f6e37
Catherine <catherine.cheu@uconn.edu>
2021-12-09 23:37:06 -0500

    Initialize new GUI version

--- space_gui_ver 2.jl
+++ space_gui_ver 2.jl
@@ -3,3 +3,9 @@ tk = pyimport("tkinter")
 mathin = pyimport("math")
 
 piinc = mathin.pi
+
+pydef mutable struct Guispace <: tk.Tk
+    __init__(self) = begin
+        tk.Tk.__init__(self)
+    end
+end
